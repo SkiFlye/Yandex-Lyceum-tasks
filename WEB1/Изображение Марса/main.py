9afd7aa4f2ce276d823d935adcebfaf00c78a667
SkiFlye <matvey.nikiforoff@mail.ru>
2026-02-17 17:57:03 +0500

    added a solution Image of Mars

---
+++ WEB1/Изображение Марса/main.py
@@ -0,0 +1,45 @@
+from flask import Flask, url_for
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def mission():
+    return "Миссия Колонизация Марса"
+
+
+@app.route('/index')
+def motto():
+    return "И на Марсе будут яблони цвести!"
+
+
+@app.route('/promotion')
+def promotion():
+    promotion_lines = [
+        "Человечество вырастает из детства.",
+        "Человечеству мала одна планета.",
+        "Мы сделаем обитаемыми безжизненные пока планеты.",
+        "И начнем с Марса!",
+        "Присоединяйся!"
+    ]
+    return "</br>".join(promotion_lines)
+
+
+@app.route('/image_mars')
+def image_mars():
+    return f'''<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <title>Привет, Марс!</title>
+                  </head>
+                  <body>
+                    <h1>Жди нас, Марс!</h1>
+                    <img src="{url_for('static', filename='images/mars.png')}" >
+                    <p>Вот она какая, красная планета!</p>
+                  </body>
+                </html>'''
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
