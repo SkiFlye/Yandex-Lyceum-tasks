b3acf769e9b61a319c517d68aedc5419481cb89c
SkiFlye <matvey.nikiforoff@yandex.ru>
2026-02-18 16:04:46 +0500

    Solution for the Selection result Task has been added

--- WEB1/Отбор астронавтов/main.py
+++ WEB1/Отбор астронавтов/main.py
@@ -179,5 +179,31 @@ def choice(planet_name):
                 </html>'''
 
 
+@app.route('/results/<nickname>/<int:level>/<float:rating>')
+def results(nickname, level, rating):
+    return f'''<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1">
+                    <title>Результаты отбора</title>
+                    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css">
+                  </head>
+                  <body>
+                    <div class="container mt-4">
+                        <h1>Результаты отбора</h1>
+                        <h2>Претендента на участие в миссии {nickname}:</h2>
+                        <div class="alert alert-success mt-3" role="alert">
+                            Поздравляем! Ваш рейтинг после {level} этапа отбора
+                        </div>
+                        составляет {rating}!
+                        <div class="alert alert-warning" role="alert">
+                            Желаем удачи!
+                        </div>
+                    </div>
+                  </body>
+                </html>'''
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
