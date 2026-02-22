233d1f590c5a850c761f1cc3a7aaf258af5d68bd
SkiFlye <matvey.nikiforoff@yandex.ru>
2026-02-22 12:08:22 +0500

    Solution for the Personal card has been added

---
+++ WEB2/Личная карточка/main.py
@@ -0,0 +1,121 @@
+import os
+import json
+import random
+from flask import Flask, render_template, redirect, request
+from forms import EmergencyAccessForm
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+@app.route('/')
+def index_root():
+    return render_template('base.html', title='Главная')
+
+
+@app.route('/index/<title>')
+def index_title(title):
+    return render_template('base.html', title=title)
+
+
+@app.route('/training/<prof>')
+def training(prof):
+    return render_template('training.html', title='Тренировки', prof=prof)
+
+
+@app.route('/list_prof/<list_type>')
+def list_prof(list_type):
+    professions = ['инженер', 'пилот', 'строитель', 'биолог', 'врач']
+    return render_template('list_prof.html',
+                           title='Список профессий',
+                           professions=professions,
+                           list_type=list_type)
+
+
+@app.route('/answer')
+@app.route('/auto_answer')
+def auto_answer():
+    user_data = {
+        'title': 'Анкета',
+        'surname': 'Иванов',
+        'name': 'Иван',
+        'education': 'высшее',
+        'profession': 'инженер',
+        'sex': 'мужской',
+        'motivation': 'Хочу колонизировать Марс',
+        'ready': 'true'}
+    return render_template('auto_answer.html', **user_data)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = EmergencyAccessForm()
+    if form.validate_on_submit():
+        return redirect('/success')
+    return render_template('login.html',
+                           title='Аварийный доступ',
+                           form=form)
+
+
+@app.route('/success')
+def success():
+    return "Доступ предоставлен"
+
+
+@app.route('/distribution')
+def distribution():
+    astronauts = ['Риддик', 'Железный человек', 'Чужой', 'Капитан Америка', 'Тор']
+    return render_template('distribution.html',
+                           title='Размещение по каютам',
+                           astronauts=astronauts)
+
+
+@app.route('/table_param/<sex>/<int:age>')
+def table_param(sex, age):
+    if sex == 'male':
+        if age < 21:
+            color = '#ADD8E6'
+        else:
+            color = '#4169E1'
+    else:
+        if age < 21:
+            color = '#FFB6C1'
+        else:
+            color = '#FF0000'
+    martian_type = 'child' if age < 21 else 'adult'
+    return render_template('table.html',
+                           title='Цвет каюты',
+                           sex=sex,
+                           age=age,
+                           color=color,
+                           martian_type=martian_type)
+
+
+@app.route('/carousel', methods=['GET', 'POST'])
+def carousel():
+    carousel_path = 'static/img/carousel'
+    if request.method == 'POST':
+        if 'file' in request.files:
+            file = request.files['file']
+            if file.filename != '':
+                file.save(os.path.join(carousel_path, file.filename))
+                return redirect('/carousel')
+    carousel_images = []
+    if os.path.exists(carousel_path):
+        carousel_images = [f for f in os.listdir(carousel_path)
+                           if f.lower().endswith(('.png', '.jpg', '.jpeg', '.gif'))]
+    return render_template('carousel.html',
+                           title='Галерея Марса',
+                           images=carousel_images)
+
+
+@app.route('/member')
+def member():
+    with open('templates/member.json', 'r', encoding='utf-8') as f:
+        crew = json.load(f)
+    member = random.choice(crew)
+    return render_template('member.html', member=member)
+
+
+if __name__ == '__main__':
+    app.run(port=8080)
