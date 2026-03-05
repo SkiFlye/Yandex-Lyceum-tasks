c27b4b1033809094bf465f377b6c23e7c8f5917a
SkiFlye <matvey.nikiforoff@yandex.ru>
2026-03-05 19:49:20 +0500

    Solution for the Work log has been added

---
+++ WEB3/Форма регистрации/data/jobs.py
@@ -0,0 +1,17 @@
+import datetime
+import sqlalchemy as sa
+from sqlalchemy import orm
+from .db_session import SqlAlchemyBase
+
+
+class Jobs(SqlAlchemyBase):
+    __tablename__ = 'jobs'
+    id = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
+    team_leader = sa.Column(sa.Integer, sa.ForeignKey("users.id"))
+    job = sa.Column(sa.String, nullable=True)
+    work_size = sa.Column(sa.Integer, nullable=True)
+    collaborators = sa.Column(sa.String, nullable=True)
+    start_date = sa.Column(sa.DateTime, default=datetime.datetime.now)
+    end_date = sa.Column(sa.DateTime, nullable=True)
+    is_finished = sa.Column(sa.Boolean, default=False)
+    user = orm.relationship('User')
---
+++ WEB3/Форма регистрации/main.py
@@ -0,0 +1,58 @@
+import os
+from flask import Flask, render_template, redirect
+from data import db_session
+from data.jobs import Jobs
+from data.users import User
+from forms.user import RegisterForm
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+@app.route("/")
+def index():
+    db_sess = db_session.create_session()
+    jobs = db_sess.query(Jobs).all()
+    return render_template("index.html", jobs=jobs)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            surname=form.surname.data,
+            name=form.name.data,
+            age=form.age.data,
+            position=form.position.data,
+            speciality=form.speciality.data,
+            address=form.address.data,
+            email=form.email.data
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/')
+    return render_template('register.html', title='Регистрация', form=form)
+
+
+def main():
+    db_file = "db/mars_explorer.db"
+    db_dir = os.path.dirname(db_file)
+    if db_dir and not os.path.exists(db_dir):
+        os.makedirs(db_dir)
+    db_session.global_init(db_file)
+    app.run()
+
+
+if __name__ == '__main__':
+    main()
